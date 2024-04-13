bb6bbf01cad3d71ee9e644737757c58a461a0eb8
Gavin Morgan <morgan.gavin@gmail.com>
2024-04-13 14:34:16 -0700

    proper ES target

--- src/opml.ts
+++ src/opml.ts
@@ -1,9 +1,8 @@
 //@ts-expect-error
-import opml from 'opml'
-import {promisify} from "es6-promisify"
-const parse = promisify(opml.parse)
+import { parse } from 'opml'
+import { promisify } from 'util'
 
-export async function extractURLs(OPML: string): Promise<string[]>{
-    const document = await parse(OPML).catch(() => null)
-    return document?.opml.body.subs?.[0].subs?.map(({xmlUrl}:{xmlUrl:string})=>xmlUrl) || []
+export async function extractURLs(OPML: string): Promise<string[]> {
+    const document = await promisify(parse)(OPML).catch(() => null)
+    return document?.opml.body.subs?.[0].subs?.map(({ xmlUrl }: { xmlUrl: string }) => xmlUrl) || []
 }
