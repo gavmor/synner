b18fca1e4db60488ec53d24782da81fb6767c337
Gavin Morgan <morgan.gavin@gmail.com>
2024-04-12 17:23:27 -0700

    failing test: promises

--- src/opml.test.ts
+++ src/opml.test.ts
@@ -1,8 +1,36 @@
 import {test, expect, equals} from "@benchristel/taste"
 import {extractURLs} from "./opml.ts"
 
+const OPMLFixture = `<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<opml version="1.0">
+    <head>
+        <title>Pocket Casts Feeds</title>
+    </head>
+
+    <body>
+        <outline text="feeds">
+            <outline
+                xmlUrl="https://feeds.megaphone.fm/bloodandsyrup"
+                type="rss"
+                text="Blood &amp; Syrup: A Vampire the Masquerade Podcast"
+            />
+
+
+            <outline
+                xmlUrl="https://primalblueprint.libsyn.com/rss"
+                type="rss"
+                text="The Primal Kitchen Podcast"
+            />
+        </outline>
+    </body>
+</opml>
+`
+
 test("Sinner given OPML XML", {
     "extracts URLs"(){
-        expect(extractURLs(foo), equals, []) 
+        expect(extractURLs(OPMLFixture), equals, [
+            "https://feeds.megaphone.fm/bloodandsyrup",
+            "https://primalblueprint.libsyn.com/rss"
+        ]) 
     }
 })
--- src/opml.ts
+++ src/opml.ts
@@ -1,1 +1,8 @@
-export function extractURLs(){}
+import opml from 'opml'
+import {promisify} from "es6-promisify"
+const parse = promisify(opml.parse)
+
+export async function extractURLs(OPML: string): Promise<string[]>{
+    const document = await parse(OPML)
+    return document.opml.body.subs[0].subs.map(sub=>sub.xmlUrl)
+}
